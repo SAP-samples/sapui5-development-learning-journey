40e6d104449b35c94737bd5baf3ef4417f1265b9
Christoph Knecht <christoph.knecht@sap.com>
2022-07-23 22:22:04 +0000

    add App view

--- webapp/index.js
+++ webapp/index.js
@@ -1,6 +1,11 @@
-sap.ui.define(["sap/m/Text"], function (Text) {
+sap.ui.define(["sap/ui/core/mvc/XMLView"], function (XMLView) {
     "use strict";
 
-    new Text({ text: "Hello World" }).placeAt("content");
+    XMLView.create({
+        id: "App",
+        viewName: "sap.training.exc.view.App"
+    }).then(function (oView) {
+        oView.placeAt("content");
+    });
 
 });
