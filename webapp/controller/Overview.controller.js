ce0fef017a641d9df005b96b1524fcb28f85d7e0
Christoph Knecht <christoph.knecht@sap.com>
2022-07-25 10:15:05 +0000

    add fragment

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -1,14 +1,36 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/syncStyleClass"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, syncStyleClass) {
         "use strict";
 
         return Controller.extend("sap.training.exc.controller.Overview", {
 
+            onSave: function () {
+                // create dialog lazily
+                if (!this.pDialog) {
+                    this.pDialog = this.loadFragment({
+                        name: "sap.training.exc.view.Dialog"
+                    }).then(function (oDialog) {
+                        // forward compact/cozy style into dialog
+                        syncStyleClass(this.getOwnerComponent().getContentDensityClass(), this.getView(), oDialog);
+                        return oDialog;
+                    }.bind(this));
+                }
+                this.pDialog.then(function (oDialog) {
+                    oDialog.open();
+                });
+            },
+
+            onCloseDialog: function () {
+                // note: You don't need to chain to the pDialog promise, since this event handler
+                // is only called from within the loaded dialog itself.
+                this.byId("dialog").close();
+            }
 
         });
     });
