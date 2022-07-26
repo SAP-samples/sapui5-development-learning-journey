113a9c7042261db9ba883e0831b63ae63001d5a0
Christoph Knecht <christoph.knecht@sap.com>
2022-07-26 13:53:30 +0000

    add back navigation

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,15 +1,26 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, History) {
         "use strict";
 
         return Controller.extend("sap.training.exc.controller.Detail", {
 
+            onNavBack: function () {
+                var oHistory = History.getInstance();
+                var sPreviousHash = oHistory.getPreviousHash();
 
+                if (sPreviousHash !== undefined) {
+                    window.history.go(-1);
+                } else {
+                    var oRouter = this.getOwnerComponent().getRouter();
+                    oRouter.navTo("overview", {}, true);
+                }
+            }
 
         });
     });
